19c65a4b0a7b500c8c1dac1482b59e17698d714b
Jack <jackwong.professional@gmail.com>
2019-07-31 08:27:27 -0400

    tweaked animations again

--- colorGame/UIButtonExtension.swift
+++ colorGame/UIButtonExtension.swift
@@ -42,7 +42,7 @@ extension UIButton {
         
         let shake = CABasicAnimation(keyPath: "position")
         shake.duration = 0.1
-        shake.repeatCount = 1
+        shake.repeatCount = 3
         shake.autoreverses = true
         
         let fromPoint = CGPoint(x: center.x - 5, y: center.y)
