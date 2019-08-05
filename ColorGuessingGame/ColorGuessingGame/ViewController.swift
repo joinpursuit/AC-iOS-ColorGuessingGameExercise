2a1735f8cf4f5ec2a3db69de91f6b1055dfba8d4
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-05 14:48:00 -0400

    complete

--- ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -12,18 +12,106 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var randomColorView: UIView!
     
-    @IBAction func buttonPressed(_ sender: Any) {
-        colorChange.
+    @IBOutlet weak var reusltsLabel: UILabel!
+    
+    @IBOutlet weak var currentScore: UILabel!
+    
+    @IBOutlet weak var highScore: UILabel!
+    
+    @IBOutlet var allButtons: [UIButton]!
+    
+    func disableButtons(){
+        allButtons.forEach({$0.isEnabled = false})
     }
     
+ var model = colorChange ()
+ var score = 0
+ var scoreModel = scoreKeeping()
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        scoreModel.resetScore()
+        //reenable the button
+        currentScore.text = String(scoreModel.currentScore)
+        allButtons.forEach({$0.isEnabled = true})
+        //closure used above*
+    }
+    
+    func nextPlayerTurn (){
+       model = colorChange()
+        randomColorView.backgroundColor = model.randoColorGen()
+        
+    }
+    func updateLabel(winOrLose: GameResult) {
+        switch winOrLose {
+        case .win :
+            //scoreModel.updateScore()
+            reusltsLabel.text = "You win"
+            currentScore.text! = "Current Score:  \(scoreModel.currentScore)"
+            highScore.text = "High Score:\(scoreModel.highScore)"
+           
+        case .lose:
+            reusltsLabel.text = "You lose"
+            
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        
+        switch sender.tag{
+        case 0:
+            if model.getDominantColor() == UIColor.red {
+                updateLabel(winOrLose: .win)
+                nextPlayerTurn()
+                scoreModel.updateScore()
+                 currentScore.text! = "Current Score:  \(scoreModel.currentScore)"
+                highScore.text = "High Score:\(scoreModel.highScore)"
+                
+            } else {
+                 updateLabel(winOrLose: .lose)
+                nextPlayerTurn()
+                disableButtons()
+                
+            }
+        case 1:
+            if model.getDominantColor() == UIColor.green {
+                updateLabel(winOrLose: .win)
+                nextPlayerTurn()
+                scoreModel.updateScore()
+                 currentScore.text! = "Current Score:  \(scoreModel.currentScore)"
+                highScore.text = "High Score:\(scoreModel.highScore)"
+            } else {
+                updateLabel(winOrLose: .lose)
+                nextPlayerTurn()
+               disableButtons()
+            }
+        case 2:
+            if model.getDominantColor() == UIColor.blue {
+                updateLabel(winOrLose: .win)
+                nextPlayerTurn()
+                scoreModel.updateScore()
+                 currentScore.text! = "Current Score:  \(scoreModel.currentScore)"
+                highScore.text = "High Score:\(scoreModel.highScore)"
+            } else {
+                updateLabel(winOrLose: .lose)
+                nextPlayerTurn()
+                disableButtons()
+            }
+           
+        default:
+            print("this will never happen")
+        }
+        
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       randomColorView.backgroundColor = model.randoColorGen()
+//        score = scoreModel.currentScore
+       // scoreModel.updateScore()
     }
 
 
 }
-
--- ColorGuessingGame/ColorGuessingGame/model.swift
+++ ColorGuessingGame/ColorGuessingGame/model.swift
@@ -9,16 +9,48 @@
 import Foundation
 import UIKit
 
-struct colorChange{
-    var red: CGFloat = 0.0
-    var blue: CGFloat = 0.0
-    var green: CGFloat = 0.0
-    var alpha: CGFloat = 1.0
-    
-mutating func mixColor () -> UIColor {
-    red = CGFloat.random(in: 0...1)
-    green = CGFloat.random(in: 0...1)
-    blue = CGFloat.random(in: 0...1)
-    alpha = 1
-    let myColor = UIColor(red:red, green:green, blue:blue, )
+enum GameResult {
+    case win
+    case lose
 }
+
+struct colorChange {
+    var red: CGFloat = CGFloat.random(in: 0...1)
+    var blue: CGFloat = CGFloat.random(in: 0...1)
+    var green: CGFloat = CGFloat.random(in: 0...1)
+    
+    
+    func randoColorGen () -> UIColor {
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        return color
+    }
+    
+    func getDominantColor () -> UIColor {
+        if red > blue && red > green {
+            return UIColor.red
+        }
+       else if blue > red && blue > green {
+            return UIColor.blue
+        }
+        else {
+            return UIColor.green
+        }
+    }
+}
+
+struct scoreKeeping {
+    var highScore = 0
+    var currentScore = 0
+    
+    mutating func resetScore() {
+        currentScore = 0
+    }
+    
+    mutating func updateScore() {
+        currentScore += 1
+        if currentScore >= highScore {
+            highScore = currentScore
+        }
+    }
+}
+
