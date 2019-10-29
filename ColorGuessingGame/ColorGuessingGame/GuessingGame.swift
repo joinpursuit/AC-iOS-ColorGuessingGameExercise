91b599795e126edd8dfe635cdc0cfc1e0866a20d
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-10-29 19:23:13 -0400

    Refactored code, moved rgba function to struct to complete model stuff, moved score to model as well. Made code slightly more efficient by removing an array.

--- ColorGuessingGame/ColorGuessingGame/GuessingGame.swift
+++ ColorGuessingGame/ColorGuessingGame/GuessingGame.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 struct GuessingGame {
-    let randomColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: CGFloat.random(in: 0...1))
     static var highScore = 0
+    static var score = 0
+    
+    static func randomUIColor() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0...1),
+                       green: CGFloat.random(in: 0...1),
+                       blue: CGFloat.random(in: 0...1),
+                       alpha: CGFloat.random(in: 0...1))
+    }
+    
+    static func rgbValue(_ view: UIColor) -> Int {
+           var red: CGFloat = 0
+           var green: CGFloat = 0
+           var blue: CGFloat = 0
+           var alpha: CGFloat = 0
+           view.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+           
+           if red >= green && red >= blue {
+               return 0
+           } else if green >= red && green >= blue {
+               return 1
+           } else {
+               return 2
+           }
+       }
 }
 
--- ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -16,69 +16,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreCounter: UILabel!
     @IBOutlet weak var highScore: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
-    var score: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let firstRandomColor = GuessingGame()
-        colorView.backgroundColor = firstRandomColor.randomColor
+        colorView.backgroundColor = GuessingGame.randomUIColor()
         playAgainButton.isHidden.toggle()
     }
     
     @IBAction func colorButtonChoice(_ sender: UIButton) {
         guard let bgColor = colorView.backgroundColor else { return }
-        let changingColor = GuessingGame()
-        let x = rgbValue(bgColor)
-        if sender.tag == x {
+        if sender.tag == GuessingGame.rgbValue(bgColor) {
             displayLabel.text = "Correct!"
-            score += 1
-            scoreCounter.text = String(score)
-            colorView.backgroundColor = changingColor.randomColor
-            
-            if score > GuessingGame.highScore {
-                GuessingGame.highScore = score
+            GuessingGame.score += 1
+            scoreCounter.text = String(GuessingGame.score)
+            colorView.backgroundColor = GuessingGame.randomUIColor()
+            if GuessingGame.score > GuessingGame.highScore {
+                GuessingGame.highScore = GuessingGame.score
                 highScore.text = String(GuessingGame.highScore)
             }
         } else {
             displayLabel.text = "Wrong!"
-            for button in colorButtons {
-                button.isEnabled.toggle()
-            }
+            for button in colorButtons { button.isEnabled.toggle() }
             playAgainButton.isHidden.toggle()
         }
     }
-    @IBAction func newGame(_ sender: UIButton) {
-        let newColorChange = GuessingGame()
-        score = 0
-        scoreCounter.text = String(score)
-        colorView.backgroundColor = newColorChange.randomColor
-        displayLabel.text = "Choose a color."
-        
-        for button in colorButtons {
-            button.isEnabled.toggle()
-        }
-        
-        playAgainButton.isHidden.toggle()
-        
-    }
     
-    func rgbValue(_ view: UIColor) -> Int {
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        var alpha: CGFloat = 0
-        
-        view.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        
-        let rgbValues: [CGFloat] = [red, green, blue]
-        if red == rgbValues.max() {
-            return 0
-        } else if green == rgbValues.max() {
-            return 1
-        } else {
-            return 2
-        }
+    @IBAction func newGame(_ sender: UIButton) {
+        GuessingGame.score = 0
+        scoreCounter.text = String(GuessingGame.score)
+        displayLabel.text = "Choose a color."
+        colorView.backgroundColor = GuessingGame.randomUIColor()
+        for button in colorButtons { button.isEnabled.toggle() }
+        playAgainButton.isHidden.toggle()
     }
 }
 
