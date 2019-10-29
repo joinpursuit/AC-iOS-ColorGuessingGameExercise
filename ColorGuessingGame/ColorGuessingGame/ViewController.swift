776fab2995b003d1319052ad850254738564241f
Ahad Islam <34593133+ahadislam1@users.noreply.github.com>
2019-10-29 16:24:44 -0400

    Project complete, had multiple commits but forked from Pursuit's repo and not mine.  Oops.

---
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  ColorGuessingGame
+//
+//  Created by Ahad Islam on 10/29/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet var colorButtons: [UIButton]!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var scoreCounter: UILabel!
+    var score: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let firstRandomColor = GuessingGame()
+        colorView.backgroundColor = firstRandomColor.randomColor
+    }
+    
+    @IBAction func colorButtonChoice(_ sender: UIButton) {
+        guard let bgColor = colorView.backgroundColor else { return }
+        let changingColor = GuessingGame()
+        let x = rgbValue(bgColor)
+        if sender.tag == x {
+            displayLabel.text = "Correct!"
+            score += 1
+            scoreCounter.text = String(score)
+            colorView.backgroundColor = changingColor.randomColor
+        } else {
+            displayLabel.text = "Wrong!"
+            for button in colorButtons {
+                button.isEnabled.toggle()
+            }
+        }
+    }
+    @IBAction func newGame(_ sender: UIButton) {
+        let newColorChange = GuessingGame()
+        score = 0
+        scoreCounter.text = String(score)
+        colorView.backgroundColor = newColorChange.randomColor
+        displayLabel.text = "Choose a color."
+        
+        for button in colorButtons {
+            button.isEnabled.toggle()
+        }
+        
+        
+    }
+    
+    func rgbValue(_ view: UIColor) -> Int {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        view.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        let rgbValues: [CGFloat] = [red, green, blue]
+        if red == rgbValues.max() {
+            return 0
+        } else if green == rgbValues.max() {
+            return 1
+        } else {
+            return 2
+        }
+    }
+    
+    
+}
+
---
+++ ColorGuessingGame/ColorGuessingGame/guessGaming.swift
@@ -0,0 +1,15 @@
+//
+//  ColorGuess.swift
+//  ColorGuessingGame
+//
+//  Created by Ahad Islam on 10/29/19.
+//  Copyright © 2019 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+struct GuessingGame {
+    let randomColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: CGFloat.random(in: 0...1))
+    
+}
+
