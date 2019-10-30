ab29eb58ef0ffcc40ac09a7d0dc51470bd3a2296
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-10-30 13:52:54 -0400

    game fully functional

--- ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    // Outlets:
     
     @IBOutlet weak var colorDisplay: UIView!
     @IBOutlet weak var displayLabel: UILabel!
-    
+    @IBOutlet weak var scoreLabel: UILabel!
+
     @IBOutlet weak var red: UIButton!
     @IBOutlet weak var green: UIButton!
     @IBOutlet weak var blue: UIButton!
     
     
+
+    var randomNumArr = [CGFloat]()
+    var randRed: CGFloat = 0.0
+    var randGreen: CGFloat = 0.0
+    var randBlue: CGFloat = 0.0
+    var score = 0
+    var arrayOfScores: [Int] = [0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        updateViewColor()
+        
+        // randomNumArr = [CGFloat]()
+        displayLabel.text = "Match the correct color"
+        updateDisplayColor()
         
         red.backgroundColor = .red
         green.backgroundColor = .green
@@ -31,36 +45,72 @@ class ViewController: UIViewController {
     }
     
     @IBAction func guessColor(_ sender: UIButton) {
-                
-        if sender.backgroundColor == colorDisplay.backgroundColor {
+        
+        print(randomNumArr)
+        
+        let highestRandom = randomNumArr.max()
+        guard let highestColor = highestRandom
+            else {
+                return
+        }
+        var comparingTag = 0
+        
+        switch highestColor {
+        case randRed:
+            comparingTag = 0
+        case randGreen:
+            comparingTag = 1
+        case randBlue:
+            comparingTag = 2
+        default:
+            comparingTag = 2
+        }
+        
+        if sender.tag == comparingTag {
             displayLabel.text = "correct keep going"
-            updateViewColor()
+            score += 1
+            scoreLabel.text = "Highest Score: \(arrayOfScores.max() ?? 0) Current Score: \(score)"
+            
+            randomNumArr.removeAll()
+            updateDisplayColor()
         } else {
             displayLabel.text = "You Lose! try again"
+            scoreLabel.text = "Highest Score: \(arrayOfScores.max() ?? 0) Final Score: \(score)"
+            arrayOfScores.append(score)
             red.isEnabled = false
             blue.isEnabled = false
             green.isEnabled = false
-        }        
+        }
+        
     }
     
-    func updateViewColor() {
-        let randRed = CGFloat.random(in: 0...1)
-        let randGreen = CGFloat.random(in: 0...1)
-        let randBlue = CGFloat.random(in: 0...1)
+    func updateDisplayColor() {
+        randRed = CGFloat.random(in: 0...1)
+        randGreen = CGFloat.random(in: 0...1)
+        randBlue = CGFloat.random(in: 0...1)
+        
+        randomNumArr.append(randRed)
+        randomNumArr.append(randGreen)
+        randomNumArr.append(randBlue)
+        
         let myColor = UIColor(red: randRed, green: randGreen, blue: randBlue, alpha: 1)
-        
-        // print(randomColor)
-        
         colorDisplay.backgroundColor = myColor
         
-        
+
+ 
     }
     
     @IBAction func resetGame(_ sender: UIButton) {
-        updateViewColor()
+        print(arrayOfScores)
+        
+        randomNumArr.removeAll()
+        updateDisplayColor()
+        displayLabel.text = "Match the correct color"
+        score = 0
         red.isEnabled = true
         blue.isEnabled = true
         green.isEnabled = true
+        scoreLabel.text = "Score to beat : \(arrayOfScores.max() ?? 0)"
         
     }
 }
