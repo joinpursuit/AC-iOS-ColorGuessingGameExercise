a012f6385fa4430c5dcf2193c5d46e625946cec5
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-10-31 01:08:58 -0400

    Got game working. Created a class to separate some of the game logic away from the controller. Just need to debug the background color of the game since it always shows black

--- Color guessing game/Color guessing game/ViewController.swift
+++ Color guessing game/Color guessing game/ViewController.swift
@@ -15,63 +15,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayedColor: UIView!
     @IBOutlet weak var userFeedBack: UILabel!
     
-    let redRandom = CGFloat.random(in: 0.0...1.0)
-    let greenRandom = CGFloat.random(in: 0.0...1.0)
-    let blueRandom = CGFloat.random(in: 0.0...1.0)
-    let alphaRandom = CGFloat.random(in: 0.0...1.0)
-    
-    //found solution on https://stackoverflow.com/questions/45423321/cannot-use-instance-member-within-property-initializer but don't understand why randomColor can be used across all functions
-    lazy var randomColor = UIColor(red: redRandom, green: greenRandom, blue: blueRandom, alpha: alphaRandom)
-    
     var score = 0
-    var attempts = 0
+    var scoresOfGame = [Int]()
+    //var highestScore = 0
+    //found solution on https://stackoverflow.com/questions/45423321/cannot-use-instance-member-within-property-initializer but don't understand why randomColor can be used across all functions
+    
+    lazy var bgColor = displayedColor.backgroundColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        displayedColor.backgroundColor = randomColor
+        bgColor = ColorModel.genRandomColor()
+        //userFeedBack.isHidden = true
     }
-    
-    
     
     @IBAction func guessColor(_ guessColor: UIButton) {
-        
-        if guessColor.tag == getMaxColor() {
+        //userFeedBack.isHidden.toggle()
+        if guessColor.tag == ColorModel.getMaxColor(bgColor: bgColor) {
             score+=1
-            attempts+=1
-            userFeedBack.text = "Correct guess. Your score is \(score)"
+            scoresOfGame.append(score)
+            userFeedBack.text = "Correct guess. Your score is \(score). Your highest score is \(scoresOfGame.max()!)"
+            bgColor = ColorModel.genRandomColor()
         } else  {
-            attempts+=1
-            userFeedBack.text = "Incorrect guess. Your score is \(score)"
+            score = 0
+            scoresOfGame.append(score)
+            userFeedBack.text = "Incorrect guess. Your score is \(score). Your highest score is \(scoresOfGame.max()!)"
         }
-        
-        
     }
     
-    func getMaxColor () -> Int {
-        
-        var red = CGFloat() // 0
-        var green = CGFloat() // 1
-        var blue = CGFloat() // 2
-        var alpha = CGFloat()
-        var color = [CGFloat]()
-        
-        //how to refractor this code
-        randomColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        color.append(red)
-        color.append(green)
-        color.append(blue)
-        
-        var maxColorIndex = -1
-        
-        //get index of max color
-        for (index, rgb) in color.enumerated() {
-            if rgb == color.max() {
-                maxColorIndex = index
-            }
-        }
-        return maxColorIndex
+    @IBAction func resetGame(_ sender: UIButton) {
+        score = 0
+        scoresOfGame.removeAll()
+        userFeedBack.text = "Unknown guess. Your score is 0 and you highest score is 0."
     }
+    
     
 }
 
